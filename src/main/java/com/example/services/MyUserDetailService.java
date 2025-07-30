eceea72ac186cb55fba836ca0b65e076db66af02
tomas <gandalf@lord-of-the-lines.com>
2025-07-30 08:40:33 +0200

    Initial commit

---
+++ src/main/java/com/example/services/MyUserDetailService.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.example.entities.Role;
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MyUserDetailService implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public MyUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        for (Role role : user.getRoles()) {
+            SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role.getName());
+            authorities.add(simpleGrantedAuthority);
+        }
+        return new org.springframework.security.core.userdetails.User(username, user.getPassword(), authorities);
+    }
+}
